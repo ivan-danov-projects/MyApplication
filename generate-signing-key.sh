a2879f36406e50d633197ebaab6ec77d9c7891dd
Ivan Danov <idanov@gmail.com>
2024-01-21 21:50:51 +0200

    fixes

--- generate-signing-key.sh
+++ generate-signing-key.sh
@@ -6,16 +6,16 @@ APP=MyApplication
 KEY_PASS=password
 STORE_PASS=password
 
-# keytool -genkeypair \
-# 	-dname "cn=${APP}, ou=Security, o=${OWNER}, c=${COUNTRY}" \
-# 	-alias "${APP}Key" \
-# 	-keyalg  RSA  -keypass ${KEY_PASS} \
-# 	-keystore ${OWNER}.keystore -storepass ${STORE_PASS} \
-# 	-validity 11000
-#
-# keytool -list -keystore ${OWNER}.keystore -storepass ${STORE_PASS}
-#
-# base64 ${OWNER}.keystore > ${OWNER}.keystore.base64
+keytool -genkeypair \
+	-dname "cn=${APP}, ou=Security, o=${OWNER}, c=${COUNTRY}" \
+	-alias "${APP}Key" \
+	-keyalg  RSA  -keypass ${KEY_PASS} \
+	-keystore ${OWNER}.keystore -storepass ${STORE_PASS} \
+	-validity 11000
+
+keytool -list -keystore ${OWNER}.keystore -storepass ${STORE_PASS}
+
+base64 ${OWNER}.keystore > ${OWNER}.keystore.base64
 
 echo "Set GITHUB SECRETS"
 echo "JKS: " $(cat ${OWNER}.keystore.base64)
