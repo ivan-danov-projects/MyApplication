8aef1629f83f4652acb5d30343b856d871ab08bd
Ivan Danov <idanov@gmail.com>
2024-02-22 13:55:41 +0200

    ---

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.java
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.home;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.example.myapplication.BuildConfig;
 
 public class HomeViewModel extends ViewModel {
 
@@ -10,7 +11,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Version " + BuildConfig.VERSION_NAME);
     }
 
     public LiveData<String> getText() {
